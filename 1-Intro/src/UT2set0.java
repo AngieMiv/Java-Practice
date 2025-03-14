d3a6f6ec29a163a1ad4713ae635109d1006e4a38
AngieMiv <angieibarrola@gmail.com>
2025-03-14 11:49:37 +0100

    local intro added to origin

---
+++ 1-Intro/src/UT2set0.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class UT2set0 {
+	
+	/** 201 Determine the type of triangle by requesting the 3 sides: a, b, c.
+	 * Equilateral Triangle: All three sides are equal.
+	 * Right Triangle: 𝑎2 + 𝑏2 = 𝑐2 (you must check all three combinations).
+	 * Isosceles Triangle: Two sides are equal (all cases must be checked).
+	 * If none of the above conditions are met, print "Scalene Triangle".
+	 */
+	public static String typeOfTriangle(int a, int b, int c) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the length of each side: a, b and c. "
+				+ "First, side a: ");
+		a = sc.nextInt();
+		System.out.println("side b: ");
+		b = sc.nextInt();
+		System.out.println("side c: ");
+		c = sc.nextInt();
+		
+		
+		
+		
+		
+
+	
+		return "hola";
+	}
+
+}
